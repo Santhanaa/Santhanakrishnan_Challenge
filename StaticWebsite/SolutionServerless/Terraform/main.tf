24ce7adb9cd25e8d4c204e3fdd86e0d02e1e47d9
Santhanaa <santhanaa@gmail.com>
2024-04-08 16:54:11 +0000

    upd

---
+++ StaticWebsite/SolutionServerless/Terraform/main.tf
@@ -0,0 +1,77 @@
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my-bucket"
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_object" "object" {
+  bucket = "my-bucket"
+  key    = "index.html"
+  source = "index.html"
+  acl    = "public-read"
+}
+
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "S3 origin identity for my CloudFront distribution"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+    origin_id   = "myS3Origin"
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "S3 Distribution for My Bucket"
+  default_root_object = "index.html"
+  price_class         = "PriceClass_100"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "myS3Origin"
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "www"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_cloudfront_distribution.s3_distribution.domain_name]
+}
+
+resource "aws_route53_zone" "primary" {
+  name = "my-domain.com"
+}
